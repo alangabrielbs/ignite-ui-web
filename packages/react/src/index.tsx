e6794c9a7f1ece87ab896e62f31cbe030625b0b1
Alan Gabriel <contato@alangabriel.dev>
2022-10-07 14:30:02 -0300

    add stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  color: '$black',
+  backgroundColor: '$white',
+})
+
+const App = () => <Button>Hello world</Button>
+
+export default App
