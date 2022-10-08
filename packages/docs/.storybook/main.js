e365b9c9c107d311150b5af143b5b6669c8f1f83
Alan Gabriel <contato@alangabriel.dev>
2022-10-08 14:56:51 -0300

    ci: add docs workflows

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  "viteFinal": async (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-ui-web/'
+    }
+    return config;
   }
 }
